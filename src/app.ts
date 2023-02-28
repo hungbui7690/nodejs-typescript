d725ba7cf53078637887671230bd9eb7b8ac85f1
Hung Bui <hungbui7690@gmail.com>
2023-02-28 16:11:58 -0700

    Initial Setup for Typescript in NodeJS

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+/*
+  - npm install typescript -D
+  - npx tsc --init : create tsconfig.json
+  - npm i ts-node-dev --save-dev : similar to nodemon
+  - package.json: 
+    + "dev": "ts-node-dev --respawn --transpile-only src/app.ts" > docs 
+
+*/
+
+console.log('Hello World')
