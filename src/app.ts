248c02b2ef28d35c36940f44132b3c5dd5d482e4
Hung Bui <hungbui7690@gmail.com>
2023-02-28 16:29:27 -0700

    Add type for req & res

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,25 @@
 /*
-  - npm install express
-  - npm i @types/node
-  - npm install --save @types/express
 
 */
 
-import express from 'express'
+import express, { Request, Response } from 'express'
 
 const app = express()
 
-// curl http://localhost:3000/
-app.get('/', (req, res) => {
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
   return res.send('Hello World')
+})
+
+app.post('/api/data', (req: Request, res: Response) => {
+  console.log(req.body)
+
+  return res.sendStatus(200)
+})
+
+app.all('/api/all', (req: Request, res: Response) => {
+  return res.status(200).send('All')
 })
 
 app.listen(3000, () => console.log(`Server is listening on port 3000...`))
