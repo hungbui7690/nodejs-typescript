8a6cfffffc09d2c27b93848b12c2b62004ad6aae
Hung Bui <hungbui7690@gmail.com>
2023-02-28 21:30:06 -0700

    Folder Structure (2 methods)

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,17 @@
 /*
+  Folder Structure:
+  + Method 1:
+    - Services/ : use to connect to database > Services will import models
+    > Services import Models > Controllers import Services > Routes import Controllers
 
+  + Method 2: this is the way NextJS use
+    - src/books: 
+      + books.controller.ts
+      + books.models.ts
+      + books.service.ts
+    - src/:
+      + app.ts
+      + routes.ts
 */
 
 import express, { Request, Response, NextFunction } from 'express'
