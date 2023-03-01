34ad307198804f1e5401e4b7c086909d5e9df7f0
Hung Bui <hungbui7690@gmail.com>
2023-02-28 18:06:40 -0700

    Middleware P2

--- src/app.ts
+++ src/app.ts
@@ -6,17 +6,30 @@ import express, { Request, Response, NextFunction } from 'express'
 const app = express()
 app.use(express.json())
 
-// (1)
 const handleGetBookOne = (req: Request, res: Response, next: NextFunction) => {
-  console.log('First Middleware')
-  next() // (***) must have
+  // @ts-ignore: ignore typescript check for the next line
+  req.name = 'John'
+  next()
 }
 const handleGetBookTwo = (req: Request, res: Response, next: NextFunction) => {
-  console.log('Second Middleware')
+  // @ts-ignore
+  console.log(req.name)
   res.send(req.params)
 }
 
-// (2)
 app.get('/books/:bookID', [handleGetBookOne, handleGetBookTwo])
+
+// (***) we can also write like this > but it looks like a mess
+app.get(
+  '/books/:bookID/:authorID',
+  (req: Request, res: Response, next: NextFunction) => {
+    console.log('First Middleware')
+    next()
+  },
+  (req: Request, res: Response, next: NextFunction) => {
+    console.log('Second Middleware')
+    res.send(req.params)
+  }
+)
 
 app.listen(3000, () => console.log(`Server is listening on port 3000...`))
