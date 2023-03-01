9ebbc9eb5f3d6c9f3123e98e9412b50644f7165a
Hung Bui <hungbui7690@gmail.com>
2023-02-28 17:53:47 -0700

    Dynamic Path P2

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,13 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/health', (req, res) => res.send('/health'))
-app.get('/ab*cd', (req, res) => res.send('/ab*cd')) // /abcd, /abxcd, /abRANDOMcd
-app.get('/abc|bcd/', (req, res) => res.send('/abc|bcd/')) // /abc, /bcd
+app.get('/books/:bookID', (req, res) => {
+  console.log(req.params.bookID)
+  res.json(req.params)
+})
+
+app.get('/books/:bookID/:authorID', (req, res) => {
+  res.json(req.params)
+})
 
 app.listen(3000, () => console.log(`Server is listening on port 3000...`))
