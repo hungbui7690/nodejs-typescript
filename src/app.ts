7ee6db2f3acd0b7de5b6ec0ac0b1fe87679ab04e
Hung Bui <hungbui7690@gmail.com>
2023-02-28 20:10:00 -0700

    Generic Types

--- src/app.ts
+++ src/app.ts
@@ -6,23 +6,33 @@ import express, { Request, Response, NextFunction } from 'express'
 const app = express()
 app.use(express.json())
 
-// (1) if we want to pass parameter > we need to return a function
-// > this technique is called "currying"
 const middleware =
   ({ name }: { name: string }) =>
   (req: Request, res: Response, next: NextFunction) => {
-    // @ts-ignore: ignore typescript check for the next line
-    req.name = name
+    res.locals.name = name
     next()
   }
 
-// (2) pass argument
 app.use(middleware({ name: 'John Doe' }))
 
-app.get('/books/:bookID', (req: Request, res: Response) => {
-  // @ts-ignore
-  console.log(req.name)
-  res.send('Hello There')
-})
+// (***) Generic Types > check pic > when we hover on Request type > it shows the types with parameters: <{params}, {res}, {req}...>
+// > with the below implementation, we can handle the user input > user still can put the other stuffs in req.body > but when we use here, it will return errors
+app.post(
+  '/books/:bookID',
+  (
+    req: Request<
+      { name: 'string'; authorId: 'string' }, // now we can access req.params.name / authorId
+      {},
+      { name: 'string' }, // we can access req.body.name
+      {}
+    >,
+    res: Response
+  ) => {
+    const { name } = req.body // we just can extract req.body.name here
+    // @ts-ignore
+    console.log(res.locals.name)
+    res.send('Hello There')
+  }
+)
 
 app.listen(3000, () => console.log(`Server is listening on port 3000...`))
